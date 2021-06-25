32178361e453f900221ac570d791f7ca786aceb9
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-06-24 20:25:09 -0400

    placement functional testing

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -3,8 +3,10 @@ const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
 let solver = new Solver;
+solver.puzzleString = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
+solver.generateBoard(solver.puzzleString);
 
-suite('UnitTests', () => {
+suite('UnitTests', () => {   
 
     suite('Logic for handling string input', function(){
         test('Logic handles a valid puzzle string of 81 characters', function(){
@@ -17,6 +19,33 @@ suite('UnitTests', () => {
     
         test('Logic handles a puzzle string that is not 81 characters in length', function(){
             assert.equal(solver.validate('1.5..2.84.6....926914.37.'), 'invalid length');
+        });
+    });    
+
+    suite('Logic for row placement', function(){        
+        test('Logic handles a valid row placement', function(){
+            assert.equal(solver.checkRowPlacement(solver.board,8,4,5), 'valid');
+        })
+        test('Logic handles an invalid row placement', function(){
+            assert.equal(solver.checkRowPlacement(solver.board,0,1,4), 'invalid row');
+        })
+    });
+
+    suite('Logic for column placement', function(){
+        test('Logic for a valid column placement', function(){
+            assert.equal(solver.checkColPlacement(solver.board,0,1,5), 'valid');
+        });
+        test('Logic for an invalid column placement', function(){
+            assert.equal(solver.checkColPlacement(solver.board,0,2,1), 'invalid column');
+        });
+    });
+    
+    suite('Logic for region placement', function(){
+        test('Logic for a valid region placement', function(){
+            assert.equal(solver.checkRegionPlacement(solver.board,8,4,5), 'valid');
+        });
+        test('Logic for an invalid region placement', function(){
+            assert.equal(solver.checkRegionPlacement(solver.board,8,4,8), 'invalid region');
         });
     });
 });
